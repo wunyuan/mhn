c2efb87d0ebd89e540fe5d805425bb6e9e85d497
wunyuan <wenyan06@gmail.com>
2023-11-13 13:46:51 +0800

    Update install_mongodb_ub22.sh
    update libssl version

--- scripts/install_mongodb_ub22.sh
+++ scripts/install_mongodb_ub22.sh
@@ -14,8 +14,8 @@ apt update
 #wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.13_amd64.deb
 #dpkg -i ./libssl1.0.0_1.0.2n-1ubuntu5.13_amd64.deb
 
-#wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
-#dpkg -i ./libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
+dpkg -i ./libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
 
 #apt update
 #apt install -y mongodb-org
