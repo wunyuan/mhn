18a433658d2ff214e1123a9805fb22425fdb8b6b
wunyuan <wenyan06@gmail.com>
2023-11-15 13:50:17 +0800

    Update install_mongodb_ub22.sh
    add mongodb4.4 support

--- scripts/install_mongodb_ub22.sh
+++ scripts/install_mongodb_ub22.sh
@@ -23,10 +23,18 @@ dpkg -i ./libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb
 #systemctl start mongod
 #systemctl enable mongod
 
-apt-get install gnupg curl
-curl -fsSL https://pgp.mongodb.com/server-5.0.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/mongodb-server-5.0.gpg
-echo "deb [ arch=amd64,arm64 signed-by=/etc/apt/trusted.gpg.d/mongodb-server-5.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+#apt-get install gnupg curl
+#curl -fsSL https://pgp.mongodb.com/server-5.0.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/mongodb-server-5.0.gpg
+#echo "deb [ arch=amd64,arm64 signed-by=/etc/apt/trusted.gpg.d/mongodb-server-5.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+
+#If your server is DL380G7, please install mongodb4.4. Due to mongodb5 need a server supported AVX cpu instruction.
+sudo apt-get install gnupg curl
+curl -fsSL https://pgp.mongodb.com/server-4.4.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/mongodb-server-4.4.gpg
+echo "deb [ arch=amd64,arm64 signed-by=/etc/apt/trusted.gpg.d/mongodb-server-4.4.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+
+
 apt update
+
 
 apt install -y mongodb-org
 
